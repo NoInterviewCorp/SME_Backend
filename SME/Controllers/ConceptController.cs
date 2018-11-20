197d6f893d5d16b66792fc92694f1b288229857e
Soham Sahare <sohamsahare@gmail.com>
2018-11-20 12:55:54 +0530

    First Docker file for SME

--- SME/Controllers/ConceptController.cs
+++ SME/Controllers/ConceptController.cs
@@ -19,6 +19,7 @@ namespace SME.Controllers
 
         public ConceptController(IConceptRepository repository)
         {
+            
             this.repository = repository;
         }
 
--- SME/Source/Persistence/QuestionRepository.cs
+++ SME/Source/Persistence/QuestionRepository.cs
@@ -17,24 +17,63 @@ namespace SME.Persistence
         }
 
         // Question
-        public async Task<List<Question>> AddQuestionsAsync(List<Question> questions){
+        public async Task<List<Question>> AddQuestionsAsync(List<Question> questions)
+        {
+        //     var query = graph.Cypher;
+        //     var questionParams = new ExpandoObject() as IDictionary<string, object>;
+        //     for (int i = 0; i < questions.Count; i++)
+        //     {
+        //         var question = questions[i];
+        //         if (question.QuestionId == null || question.QuestionId == "")
+        //         {
+        //             question.QuestionId = Guid.NewGuid().ToString("N");
+        //         }
+        //         questionParams.Add("question"+i,question);
+        //         query = query
+        //             .Merge("(q:Question)")
+        //             .Where((Question q) => q.QuestionId == question.QuestionId)
+        //             .OnCreate()
+        //             .Set($"q={{question{i}}}")
+        //             .WithParams(questionParams)
+        //             .Return(q => q.As<Question>());
+        //     }
+        //     var result = await query;
+        //     var list = new List<Question>(result);
+        //     return (list.Count == 0) ? null : list;
+        // }
+        // public async Task<List<Question>> GetQuestionsAsync()
+        // {
+        //     return new List<Question>(
+        //                await graph.Cypher
+        //            .Match("(q:Question)")
+        //            .ReturnDistinct(q => q.As<Question>())
+        //            .ResultsAsync
+        //     );
+        return null;
+        }
+        public async Task<List<Question>> GetQuestionsByConceptOfATechAsync(string technology, string concept)
+        {
+            var result = await graph.Cypher
+                .Match("(t:Technology)-[:EXPLAINS]->(c:Concept)-[:TESTS]->(q:Question)")
+                .Where((Technology t, Concept c) => t.Name == technology && c.Name == concept)
+                .Return(q => q.As<Question>())
+                .ResultsAsync;
+            var list = new List<Question>(result);
+            if (list.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return list;
+            }
+        }
+        public async Task<Question> UpdateQuestionAsync(Question question)
+        {
             return null;
         }
-        public async Task<List<Question>> GetQuestionsAsync(){
-            return new List<Question>(
-                       await graph.Cypher
-                   .Match("(q:Question)")
-                   .ReturnDistinct(q => q.As<Question>())
-                   .ResultsAsync
-            );
-        }
-        public async Task<List<Question>> GetQuestionsByConceptOfATechAsync(string Question, string concept){
-            return null;
-        }
-        public async Task<Question> UpdateQuestionAsync(Question question){
-            return null;
-        }
-        public async Task<bool> DeleteQuestionByIdAsync(string QuestionId){
+        public async Task<bool> DeleteQuestionByIdAsync(string QuestionId)
+        {
             var result = new List<Question>(await graph.Cypher
                 .Match("(q:Question {QuestionId:{id}})")
                 .WithParams(new
@@ -53,6 +92,10 @@ namespace SME.Persistence
                 .Delete("q, relation")
                 .ExecuteWithoutResultsAsync();
             return true;
+        }
+
+        public async Task<List<Question>> GetQuestionsAsync(){
+            return null;
         }
     }
 }
