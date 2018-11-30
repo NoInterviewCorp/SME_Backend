fe0e75e5f3104f3104d07fd092c6cfe60bf71980
Soham Sahare <sohamsahare@gmail.com>
2018-11-30 19:04:09 +0530

    "Attempting to fix rabbitmq errors"

--- SME/Source/Services/RabbitMQConnection.cs
+++ SME/Source/Services/RabbitMQConnection.cs
@@ -14,7 +14,7 @@ namespace SME.Services
         public RabbitMQConnection()
         {
             // this.dbConnection = dbConnection;
-            
+
             Factory = new ConnectionFactory
             {
                 HostName = "localhost",
@@ -23,25 +23,27 @@ namespace SME.Services
                 Password = "strongpassword",
                 DispatchConsumersAsync = true
             };
-	try{
-            Connection = Factory.CreateConnection();
-            Model = Connection.CreateModel();
-            Model.ExchangeDeclare("KnowldegeGraphExchange", "topic");
-            Model.QueueDeclare("Contributer_KnowledgeGraph_LearningPlan", false, false, false, null);
-            Model.QueueDeclare("Contributer_KnowledgeGraph_Resources", false, false, false, null);
-            Model.QueueDeclare("Contributer_QuizEngine_Questions", false, false, false, null);
-            Model.QueueDeclare("KnowledgeGraph_Contributer_Ids", false, false, false, null);
-            Model.QueueBind("Contributer_KnowledgeGraph_LearningPlan", ExchangeNme, "Models.LearningPlan");
-            Model.QueueBind("Contributer_KnowledgeGraph_Resources", ExchangeNme, "Models.Resource");
-            Model.QueueBind("Contributer_QuizEngine_Questions", ExchangeNme, "Send.Question");
-            Model.QueueBind("KnowledgeGraph_Contributer_Ids", ExchangeNme, "Request.Question");
-	}
-	catch(Exception e){
-		Console.WriteLine("--------------------------------------------------------------");
-		Console.WriteLine(e.Message);
-		Console.WriteLine(e.StackTrace);
+            try
+            {
+                Connection = Factory.CreateConnection();
+                Model = Connection.CreateModel();
+                Model.ExchangeDeclare("KnowldegeGraphExchange", "topic");
+                Model.QueueDeclare("Contributer_KnowledgeGraph_LearningPlan", false, false, false, null);
+                Model.QueueDeclare("Contributer_KnowledgeGraph_Resources", false, false, false, null);
+                Model.QueueDeclare("Contributer_QuizEngine_Questions", false, false, false, null);
+                Model.QueueDeclare("KnowledgeGraph_Contributer_Ids", false, false, false, null);
+                Model.QueueBind("Contributer_KnowledgeGraph_LearningPlan", ExchangeNme, "Models.LearningPlan");
+                Model.QueueBind("Contributer_KnowledgeGraph_Resources", ExchangeNme, "Models.Resource");
+                Model.QueueBind("Contributer_QuizEngine_Questions", ExchangeNme, "Send.Question");
+                Model.QueueBind("KnowledgeGraph_Contributer_Ids", ExchangeNme, "Request.Question");
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine("--------------------------------------------------------------");
-	}
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("--------------------------------------------------------------");
+            }
             // Model.QueueDeclare("QuizEngine_KnowledgeGraph_Query", false, false, false, null);
             // Model.QueueDeclare("QuizEngine_KnowledgeGraph_QuizUpdate", false, false, false, null);
             // Model.QueueDeclare("QuizEngine_UserProfile_UserData", false, false, false, null);
